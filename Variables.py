518de1635d943f5025f0b13ee00e72e5b272c819
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-02-20 12:00:00 -0500

    Coursework: Variables.py (2018-02-20)

---
+++ Variables.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#one = 1
+#two = 2
+#three = 3
+
+one,two,three=5,4,3
+#print(one)
+#print(two)
+#print(three)
+
+two = two + 1
+
+#print(one)
+#print(two)
+#print(three)
+decimal=2.2
+#3print(decimal)
+
+Name = "Aadhavan" + " is a nice boy."
+#print ( Name)
+count = 0
+print (count)
+count = count + 1
+print(count)
+count = count + 2
+print(count)
