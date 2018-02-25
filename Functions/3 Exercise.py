1d54afde1260db7085572fab19e4e6a7345b7516
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-02-25 12:00:00 -0500

    Coursework: Functions (2018-02-25)

---
+++ Functions/3 Exercise.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 19:16:16 2018
+
+@author: Sharmila
+"""
+ # write a function to accept 3 numbers, add first 2 number and subtract
+ #   third number from the sum and return it.
+def addAndSubtract(coin1, coin2, coin3):
+    ultra = coin1 + coin2 - coin3
+    return ultra ;
+
+
+a, b, c = 6, 8, 3
+ultra = addAndSubtract (a, b, c,);
+print(ultra)
