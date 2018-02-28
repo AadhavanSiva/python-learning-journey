1c4faa9f336e394f6dc12909323d8ebc81f6eb68
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-02-28 12:00:00 -0500

    Coursework: Day1End.py, Functions (2018-02-28)

---
+++ Day1End.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 19:45:48 2018
+
+@author: Sharmila
+"""
+
+a = 10
+b = 5
+c = a + b
+print (c)
---
+++ Functions/SecondExercise.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 13:09:15 2018
+
+@author: Sharmila
+"""
+
+def sayHello():
+    print("Hello")
+    
+# Function to accept a number, increment it by 2 and return it.
+# Function to accept a person's year of birth and calculate 
+#  how old is he/she
+#  year of birth -> 2009,  
+#  2010 Aug 28 - 1
+#  2011 Aug 28 - 2
+#  2012 Aug 28 - 3
+#  2018        - 9
+    
+
+# Function to accept two numbers and print those two numbers
+def subject(art,music):
+    print (art,music)
+    
+def sumOneNumber(number9):
+    print (number9)
+    
+def addOne(Number):
+    output = Number + 1
+    return output
+
+def sumTwoNumbers(number6,number7):
+    c = number6 + number7
+    print (c)
+    
+def CalculateYears(yearOfBirth):
+    print(yearOfBirth)
+
+
+def displayTwoNumbers(number1,number2):
+    print(number1,number2)
+
+def addTwo( number ):
+    fact = number + 2
+    return fact ;
+
+def tellName(name):
+    print( "Your name is " + name)
+    return
+
+#sumTwoNumbers(4,7)
+#sumOneNumber(9)
+#subject(3,7)
+y = addOne(4)
+print(y)
+d = addOne(y)
+print(d)
+#CalculateYears(2009)
+#displayTwoNumbers(7,4)
+
+#addFact = addTwo (98)
+##print (addFact)
+#name = "siva"
+#tellName(name)
+#
+#tellName("Oviya")
+#
+#tellName("Alicia")
+#
+#
+#def addFive( number ):
+#    star = number + 5
+#    return star ;
+#
+#a = 32
+#star = addFive (a);
+#print(star)
