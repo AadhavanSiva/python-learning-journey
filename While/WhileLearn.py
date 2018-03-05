0f8724ec381245ca865c7eec6a073b1a66d8c3c9
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-03-05 12:00:00 -0500

    Coursework: While (2018-03-05)

---
+++ While/WhileLearn.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 19:39:04 2018
+
+@author: Sharmila
+"""
+
+#count = 0
+#while ( count < 4):
+#    print("Aadhavan")
+#    count += 1
+#
+count = 0
+while( count < 3 ):
+    print("Oviya")
+    count += 1
+
+
+count = 3
+while (count > 0):
+    print ("Hello")
+    count -= 1
+    
+print("The End")
+      
+
