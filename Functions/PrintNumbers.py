db7e993ceba7d9190bad2ab7a35d1bd82a96b010
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-03-11 12:00:00 -0400

    Coursework: Functions (2018-03-11)

---
+++ Functions/PrintNumbers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 20:01:45 2018
+
+@author: Sharmila
+"""
+
+#number = 3
+#while (number <= 30):
+#    print(number)
+#    number += 3
+
+
+endNumberString = input("Enter the last number")
+endNumber = int(endNumberString)
+a = 0
+while (a <= endNumber):
+    print (a)
+    a += 1
+    
+#table = 37;
+#number = 1
+#while (number <= 10):
+#    print(number * table)
+#    number += 1
+    
