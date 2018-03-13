f795dfca72931d3d7d086f746c50f1df036ac117
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-03-13 12:00:00 -0400

    Coursework: Functions (2018-03-13)

---
+++ Functions/Positive.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 20:35:52 2018
+
+@author: Sharmila
+#"""
+#number= 2
+#
+#if number == 1:
+#    print ("It is odd")
+#    
+#    
+number = input ("please enter a number")
+
+
+if  int(number) >= 1:
+    print ("It is positive") 
+    
+if int(number)  <= -1:
+    print ("It is negative") 
+    
+if  int(number)  == 0:
+    print ("It is zero") 
+    
