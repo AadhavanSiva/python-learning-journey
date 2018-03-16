4080c1f4efe5b3791e8f3761447bdacef593a28c
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-03-16 12:00:00 -0400

    Coursework: Functions (2018-03-16)

---
+++ Functions/IfLearn.py3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 20:59:49 2018
+
+@author: Sharmila
+"""
+
+eggs = int(input("Please enter number of eggs"))
+if int ( eggs == 6):
+  print   ("Half dozen")
+    
+if (eggs == 12):
+    print ("Dozen")
+    
+if (eggs > 12):
+    print ("More than a dozen")
+    
---
+++ Functions/ifgreaterthan50.py
@@ -0,0 +1,32 @@
+### -*- coding: utf-8 -*-
+##
+##@author: Sharmila
+##"""
+##k = input("Enter number")
+##print(k)
+##"""
+##Created on Thu Mar 15 19:42:23 2018
+###
+###if int(k) == 100:
+##    print ("Hundred")
+##    
+##if (int(k) > 50):
+##    print (">50")
+#    
+#age =int (input ("Please enter your age"))
+#if int( age ==  9 or age == 10):
+#    print ("You are a minor")
+#    
+coin =int (input ("Please Enter Your Coins"))
+if int ( coin >= 1 and coin < 200 ):
+    
+    print ("HURRAY !!! You got one star")
+ 
+if int (coin >= 200 and coin < 400):
+    
+    print ("HURRAY !!! You got two stars")
+
+if int (coin >=400 and coin <= 600):
+    
+    print ("HURRAY !!! You got three star")
+    
