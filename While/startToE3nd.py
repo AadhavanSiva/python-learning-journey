6d677f803a48794edec29e21c93733c6d13d135c
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-04-08 12:00:00 -0400

    Coursework: Basics, While (2018-04-08)

---
+++ While/startToE3nd.py
@@ -0,0 +1,9 @@
+
+numbers = [36,42,10,8]
+names = ["Chris", "Ellen", "Daisy"]
+number = int(input("Please enter a number"))
+if (number == numbers[0]):
+    print ("Your number is found")
+else: 
+    print ("Your number is not found")
+    
