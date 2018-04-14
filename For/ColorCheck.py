bed85925aed38689e5b8080893b47b2e8a8d252c
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-04-14 12:00:00 -0400

    Coursework: Arrays, For (2018-04-14)

---
+++ For/ColorCheck.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 20:04:58 2018
+
+@author: Sharmila
+"""
+
+colors = ["Blue","Red","Purple","Orange"]
+
+userColor = input("Enter your favourite color") 
+for color in colors:
+    if (userColor == color):
+        print ("Your color is found")
+    
