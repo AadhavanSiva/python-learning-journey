c8d9204960b6f2c706d361a54a8a78e461dfb2fa
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-04-16 12:00:00 -0400

    Coursework: While (2018-04-16)

---
+++ While/ages.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 16 19:53:31 2018
+
+@author: Sharmila
+"""
+usersAge = int(input("Enter a age"))
+ages = [8, 10, 36, 42]
+
+for age in ages:
+    if (usersAge == age):
+        print ("Age is found")
---
+++ While/money.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 16 20:14:33 2018
+
+@author: Sharmila
+"""
+usersMoney = float(input("Enter your money"))
+money = [ 100.7, 68.9 , 1.4 , 6.4]
+for moneys in money:
+    if (usersMoney == moneys):
+        print ("money is found")
