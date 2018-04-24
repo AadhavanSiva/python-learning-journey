491ca31df0b2a5420ef7db02050d2337dc60eb9a
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-04-24 12:00:00 -0400

    Coursework: For (2018-04-24)

---
+++ For/shape.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 16:22:51 2018
+
+@author: Sharmila
+"""
+
+wide = 8
+Toprint = "x"
+numbers = range(1,wide   +1)
+
+for number in numbers:
+    print (Toprint * number )
+numbers = range(wide,8, -1)
+for number in numbers:
+    print (Toprint * number)                                                                                                                                                                                                                                                                 
---
+++ For/shapes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 18:09:13 2018
+
+@author: Sharmila
+"""
+
+    
+wide = 8
+Toprint = "o"
+numbers = range(wide,8, + 1)
+
+for number in numbers:
+    print (Toprint * number )
+numbers = range(wide -1 )
+
+for number in numbers:
+    print (Toprint * number)
