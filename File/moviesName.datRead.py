87d37b63198b4855122e84e5fa58e6aad44e89df
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-05-13 12:00:00 -0400

    Coursework: File (2018-05-13)

---
+++ File/moviesName.datRead.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 19:23:04 2018
+
+@author: Sharmila
+"""
+
+#
+nnamesFile = open("moviesName.dat","r")
+
+nameData = namesFile.read();
+
+print (nameData)
+
+namesFile.close()
