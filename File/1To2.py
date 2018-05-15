dc4485b8497037f3c80cf1dd3126e23f1eb57c59
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-05-15 12:00:00 -0400

    Coursework: File (2018-05-15)

---
+++ File/1To2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 13 19:45:07 2018
+
+@author: Sharmila
+"""
+index = int(input("Please enter a starting number"))
+index2 =int(input("Please enter a ending number"))
+number = index
+number2= index2
+while (number2 >= number ):
+    print (number)
+    number += 1
