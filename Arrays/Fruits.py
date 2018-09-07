43a9d46e06e5efbb4d3712889d09704879d5742a
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-09-07 12:00:00 -0400

    Coursework: Arrays (2018-09-07)

---
+++ Arrays/Fruits.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 16:11:32 2018
+
+@author: Aadhavan
+"""
+
+fruit=[ "Apple", "orange", "strawberry"]
+print ("These are my favourite fruits")
+print (fruit [1])
+print ("and")
+print (fruit [2])
+
+print ("These are my the fruits I don't like fruits")
+print (fruit [0])
