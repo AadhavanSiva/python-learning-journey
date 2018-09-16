19939263f350bfa37129bc585fc131044f0ab5cc
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-09-16 12:00:00 -0400

    Coursework: Arrays (2018-09-16)

---
+++ Arrays/ArrayExample.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 15 20:58:54 2018
+
+@author: Aadhavan
+"""
+#number = 5;
+#
+#number = 1;
+#
+#print( ("Baseball score for Holliston is"), number, ("run"))
+#
+#numbers = [ 1, 2, 3, 4, 5, 6 ]
+#print(numbers[1:2])
+#print("Baseball score for Medway is ", numbers[5], " runs ")
+#print ("Medway wins by 5 runs ")
+#
+#numbers[5] = 8;
+#print(numbers[4])
+#
+#
+#name = "Kutty"
+#names =[ "John, Darren, Abigal, Anna" ]
+
+
+scores = [0, 1, 2, 3, 4, 5]
+
+john = [0]
+Darren = [0]
+Abigal = [0]
+Anna = [0]
+
+john.append(4)
+Darren.append(5)
+Abigal.append(2)
+Anna.append(5)
+print ("john scores", john)
+print("Darren scores", Darren)
+print("Abigal scores", Abigal)
+print("Anna score", Anna)
