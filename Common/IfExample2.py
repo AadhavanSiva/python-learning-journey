91ea7a63b709f7395b65ec7ff52acf1946b3f268
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-09-17 12:00:00 -0400

    Coursework: Common (2018-09-17)

---
+++ Common/IfExample2.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 15 19:14:15 2018
+
+@author: Oviya
+"""
+
+number1 = 5
+number2 = 3
+
+
+if (number1 >= 10):
+  print("1")
+  print("2")
+  if(number2 > 5):
+        print("z")
+        print("y")
+  else:
+      if(number2 > 5):
+        print("K")
+        print("L")
+  print("3")
+else:
+    if(number2 > 5):
+        print("G")
+        print("H")
+    else:
+        print("V")
+        print("W")
+        if (number2 < 5):
+            print ("e")
+        else: 
+             print ("w")
+             print ("r")
+        print("S")
+    print("J")
+print("B")
+
+
+        
