5e605e99ae693812809ae1f542633e2050cc1d98
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-09-20 12:00:00 -0400

    Coursework: Common (2018-09-20)

---
+++ Common/IfExample3.py
@@ -0,0 +1,23 @@
+number1 = 10
+number2 = 4
+number3 = 6
+
+if (number1 >= 10):
+  print("1")
+  print("2")
+  if(number2 > 5):
+        print("z")
+        print("y")
+        if( number3 > 5):
+            print("X")
+        else:
+            print("Y")
+  else:
+      if(number3 > 5):
+        print("K")
+        print("L")
+      else:
+          print("N")
+  print("3")
+
+print("V")
