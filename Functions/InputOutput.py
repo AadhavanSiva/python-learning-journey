65e828980ed25c7aee39081138fbe58aab923b2c
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-09-21 12:00:00 -0400

    Coursework: Functions (2018-09-21)

---
+++ Functions/InputOutput.py
@@ -0,0 +1,64 @@
+ -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 10 10:38:41 2018
+
+@author: Sharmila
+"""
+
+PlayerName = input("Enter your name :")
+print(PlayerName)
+
+ageString = input("Enter your Age :")
+age = int(ageString)
+age = input("Enter your Age :")
+print(age )
+
+priceString = input("Enter value of Apple price " )
+price = float(priceString)  # Converts string to decimal
+print( price)
+
+quantityString = input("Enter Quantity of Apple " )
+quantity = int(quantityString);  # Converts string to integer
+print( quantity )
+
+totalValue = price * quantity
+
+print(totalValue)
+
+ Accept two integer numbers, add it & print the result
+
+number1 = input("please enter first number ")
+number2 = input ("please enter second number")
+result = int(number1) - int(number2)
+print (result)
+Accept one integer number and print it 
+
+number = input ("Please enter number")
+if int (number) > 10:
+    print ("greater than ten" )
+    
+#number1 = input("please enter 1st number")
+#number2 = input("please enter 2nd number")
+#
+#if int(number1) == int(number2):
+#    print ("number1 is equal to number2")
+#    
+#if int(number1) > int(number2):
+#    print (number1," is greater than ", number2)
+#
+#if int(number2) > int(number1):
+#    print (number2, " is greater than ", number1)
+    
+print("Hello\nAaadhavan")
+#print("Aadhavan")
+#if (number1) == (number2):
+#    print ("your numbers are same")
+#    
+#if (number1) != (number2):
+#    print ("your numbers are not same")
+#    
+
+ 
+    
+    
+    
