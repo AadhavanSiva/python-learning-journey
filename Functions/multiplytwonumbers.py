d411a52c8f423818fcf1779a0230475dd5016096
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-11-01 12:00:00 -0400

    Coursework: Functions (2018-11-01)

---
+++ Functions/multiplytwonumbers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 24 19:52:24 2018
+
+@author: Aadhavan
+"""
+
+# product
+
+def multiplyTwoNumbers(number1, number2):
+    product = number1 * number2
+    return product;
+    
+multiplyer = multiplyTwoNumbers(55, 3)
+print(multiplyer)
