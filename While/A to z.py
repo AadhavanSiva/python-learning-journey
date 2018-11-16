70485ba559ac4d251e1133c4c2ab9b4e2bb0b72d
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2018-11-16 12:00:00 -0500

    Coursework: While (2018-11-16)

---
+++ While/A to z.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 16 17:54:31 2018
+
+@author: Aadhavan
+"""
+
+firstNumber = int(input(" PLEASE ENTER A NUMBER ") )
+secondNumber= int(input(" PLEASE ENTER A NUMBER BIGGER THAN THE OTHER ONE YOU ENTERED "))
+while (firstNumber <= secondNumber):
+    print (firstNumber)
+    firstNumber += 1
