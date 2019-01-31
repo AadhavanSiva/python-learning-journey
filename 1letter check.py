3e462152b96edd381bef0694c1ad40920e1d6667
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2019-01-31 12:00:00 -0500

    Coursework: 1letter check.py (2019-01-31)

---
+++ 1letter check.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 17:09:32 2019
+
+@author: Aadhavan
+"""
+
+word = input("Please enter a word")
+a = "a"
+word1 = word.strip()
+first = word1[0]
+if(first == a):
+    print ("IT STARTS WITH 'a'")
