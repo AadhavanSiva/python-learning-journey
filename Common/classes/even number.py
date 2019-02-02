34683eb8227f35d50fe318eb98602124fcf32707
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2019-02-02 12:00:00 -0500

    Coursework: Common (2019-02-02)

---
+++ Common/classes/even number.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 20:32:59 2019
+
+@author: Aadhavan
+"""
+
+number = 0
+while (number <= 20):
+    print (number)
+    number += 2
---
+++ Common/classes/startsWith.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 20:39:30 2019
+
+@author: Aadhavan
+"""
+
+names = input("enter a name")
+if (names[0] == "o"):
+    print ("First letter is an o")
+else:
+    print ("First letter isn't an o")
