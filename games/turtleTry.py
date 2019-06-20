349408c283ea8c589d1d6de23bc6df8ff2292bb3
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2019-06-20 12:00:00 -0400

    Coursework: games (2019-06-20)

---
+++ games/turtleTry.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 19:36:49 2019
+
+@author: Aadhavan
+"""
+
+import turtle as t
+import time
+
+
+t.forward(15)
+t.right(25)
+t.pendown()
+
+t.forward(15)
+time.sleep(5)
+t.right(25)
+
+time.sleep(5)
+t.forward(15)
+time.sleep(5)
+
+t.right(25)
