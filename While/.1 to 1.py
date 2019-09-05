a37c189959fa6ace8496aae50371fd3ecfde8a34
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2019-09-05 12:00:00 -0400

    Coursework: While (2019-09-05)

---
+++ While/.1 to 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 17:03:51 2019
+
+@author: Aadhavan
+"""
+
+start = .0
+one   = .9
+index = 0
+while index <= one:
+    start += .1  
+    print (start)
+    index += .1
