b13173bc2b30cec540983234c9b13aa3c27abe39
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2019-09-10 12:00:00 -0400

    Coursework: Strings, games (2019-09-10)

---
+++ Strings/math quiz - Copy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 18:02:21 2019
+
+@author: Aadhavan
+"""
+
---
+++ Strings/math quiz.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 18:02:21 2019
+
+@author: Aadhavan
+"""
+
+question1 = int(input(" 9*3 "))
+
+if (question1 == 27) :
+    print("CORRECT")
+else:
+    break;
+    
+question2 = int(input(" 144/12 ")
+
+if (question2 == 12):
+    print("CORRECT")
+else:
+    break;
+    
+question3 = int(input(" 7654 + 4567 ")
+
+if question3 == 12223:
+    print("CORRECT")
+else:
+    break;
+    
+question4 = int(input(" 4 to the power 3 ")
+
+if question4 == 64:
+    print("CORRECT")
+else:
+    break;
+    
+    
