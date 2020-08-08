e3cdb2633a22dbe2a7c5da7efd64a43ba97bbcc4
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2020-08-08 12:00:00 -0400

    Coursework: Functions (2020-08-08)

---
+++ Functions/untitled0.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 20:25:36 2020
+
+@author: Aadhavan
+"""
+
+def Name_finder(names):
+   letters = "abcdefghijklm"
+    
+   for x in names:
+       position = len(x) - 1
+       last = x[position]
+       number = letters.find(last)
+       if number >= 0 :
+           print(x,names[x])
+            
+names = {
+        } 
+            
+index = 0  
+while index != 5:
+    print (index)
+    firstName = input("PLEASE ENTER A FIRST NAME")
+    lastName = input("PLEASE ENTER A LAST NAME")
+    names[firstName] = lastName
+    index += 1
+    
+Name_finder(names)
+
+
+    
